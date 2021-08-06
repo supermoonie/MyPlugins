b76ce0c79f6dcad6527e1d973c2d41040aebb0cc
wang_chao <chao.wang@guanaitong.com>
2021-08-06 17:25:26 +0800

    plugin

--- src/main/java/com/github/supermoonie/plugins/NotificationRouter.java
+++ src/main/java/com/github/supermoonie/plugins/NotificationRouter.java
@@ -1,6 +1,7 @@
 package com.github.supermoonie.plugins;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -10,24 +11,22 @@ import java.util.Map;
  * @author supermoonie
  * @since 2021/8/6
  */
+@Slf4j
 public class NotificationRouter {
 
     private static final String NOTIFICATION_ONCE_CREATE = "notification:once:create:";
 
-    private static final Map<String, TrayIcon.MessageType> MESSAGE_TYPE_MAP = new HashMap<>() {{
+    private static final Map<String, TrayIcon.MessageType> MESSAGE_TYPE_MAP = new HashMap<String, TrayIcon.MessageType>() {{
         put("info", TrayIcon.MessageType.INFO);
         put("warning", TrayIcon.MessageType.WARNING);
         put("error", TrayIcon.MessageType.ERROR);
         put(null, TrayIcon.MessageType.NONE);
     }};
 
-    public String onQuery(String request) {
-        if (request.startsWith(NOTIFICATION_ONCE_CREATE)) {
-            String req = request.substring(NOTIFICATION_ONCE_CREATE.length());
-            OnceNotificationCreateReq createReq = JSON.parseObject(req, OnceNotificationCreateReq.class);
-            TrayIcon.MessageType type = MESSAGE_TYPE_MAP.get(createReq.getType());
-            SystemTray.getSystemTray().getTrayIcons()[0].displayMessage(createReq.getTitle(), createReq.getContent(), type);
-        }
+    public String notify(String request) {
+        OnceNotificationCreateReq createReq = JSON.parseObject(request, OnceNotificationCreateReq.class);
+        TrayIcon.MessageType type = MESSAGE_TYPE_MAP.get(createReq.getType());
+        SystemTray.getSystemTray().getTrayIcons()[0].displayMessage(createReq.getTitle(), createReq.getContent(), type);
         return null;
     }
 }
